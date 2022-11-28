81731549c22f32afe2eb2bcf9b3ce9cd745291fc
Anatoliy Girak <agirak1@rambler.ru>
2022-11-28 15:40:16 +0300

    Commit

--- src/main/java/ru/rest_template/config/Config.java
+++ src/main/java/ru/rest_template/config/Config.java
@@ -1,2 +1,9 @@
-package ru.rest_template.config;public class Config {
+package ru.rest_template.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.rest_template")
+public class Config {
 }
